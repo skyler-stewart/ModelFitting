61a0ec8dd33fb2db5a05abdd13845176aaa6c3a7
Skyler Stewart <skmastew@ucsc.edu>
2020-12-22 20:30:37 -0800

    update model fitting

--- modelfitting.py
+++ modelfitting.py
@@ -1,96 +1,123 @@
-# FLMP Model Fitting
-# To Do (and other ideas)
-	# Dockerize (run reliably in any environment)
-	# Add command line support and/or GUI 
-	# Add support for data entry in multiple formats (JSON, CSV, etc.)
+'''
+modelfitting.py
+'''
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+import pandas as pd 
+from scipy.optimize import curve_fit, least_squares
+
+# Uses scipy's least squares optimization 
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
+def fitModel(params, actualvalues):
+	getRApplied = lambda p: getResiduals(actualvalues, p)
+	result = least_squares(getRApplied, params, jac='3-point', bounds=(0,1))
+	return list(result.x)
 
 # Outputs a list of predicted av values (using Bayes theorem)
-def preprocessing(a_params, v_params, a_weight = 1, v_weight = 1): 
-
-	weighted_a_params = []
-	weighted_v_params = []
-	predicted_values = []
-
-	# Weight a and v params if needed 
-	for a in a_params:
-		weighted_a_params.append(a * a_weight) 
-	for v in v_params:
-		weighted_v_params.append(v * v_weight)
-
+def generatePrediction(a_params, v_params): 
 	# Apply Bayes to all combinations of a and v params
-	for a in weighted_a_params:
-		for v in weighted_v_params:
+	predicted_values = []
+	for a in a_params:
+		for v in v_params:
 			value = (a*v) / ((a*v) + ((1-a) * (1-v)))
-			predicted_values.append(value)
+			predicted_values.append(value)	
+			#predicted_values.append(round(value, 2))	
 
-	# Return predicted av values
-	return predicted_values
+	# Return predicted values
+	return a_params + v_params + predicted_values
 
+# Compute and print residuals (actual - predicted)
+# actualdata: data we fit to
+# args: params (list of a and v)
+def getResiduals(actualdata, args): 
+	# Generate prediction 
+	args = list(args)
+	a_params = args[0:5]
+	v_params = args[5:10]
+	prediction = generatePrediction(a_params, v_params)
 
-# Defines the models we want to test (polynomial and hyperbolic tan as placeholders)
-def cubicModel(x, a, b, c, d):
-	x = np.asarray(x)
-	return a*(x**3) + b*(x**2) + c*x + d
+	# Get residuals
+	residuals = []
+	for i in range(len(actualdata)):
+		r = actualdata[i] - prediction[i] 
+		residuals.append(r)
 
-def tanhModel(x, a, b, c):
-	x = np.asarray(x)
-	return a*np.tanh(b*x) + c
+	return residuals
 
+# Gets and prints the RMSD for a list of residuals
+def getRMSD(residuals):
+	rmsd = sum([x**2 for x in residuals])/len(residuals)
+	print("RMSD ", rmsd)
+	return rmsd
 
-# Fits the model function to the data
-def fitModel(predicted_values, actual_values): 
+def drawTable(a, v, av):  
+	cols = ["A1", "A2", "A3", "A4", "A5", "None"]
+	rows = ["V1", "V2", "V3", "V4", "V5", "None"]
 
-	# Pick which model we want to use
-	model = cubicModel
+	groups = [av[i:i+5] for i in range(0, 25, 5)]
+	groups.append(a)
+	for i in range(0, 5):
+		groups[i].append(v[i])
 
-	# Set up graph 
-	plt.figure(figsize=(12, 8))
-	plt.title(model.__name__)
-	plt.xlabel('Predicted Values')
-	plt.ylabel('Actual Values')
+	table = pd.DataFrame(groups, columns = cols, index = rows)
+	print(table)
 
-	# Plot points (map predicted values to actual values)
-	plt.scatter(predicted_values, actual_values, label = 'Data', marker = '.', s = 50, color = 'black') 
-
-	# Fit the model (scipy's curve_fit should do the same thing as the Julia library) 
-	# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
-	p, pc = curve_fit(model, predicted_values, actual_values, maxfev=10000)
-
-	# Draw model 
-	x_scale = np.linspace(0,1,num=10000)
-	plt.plot(x_scale, model(x_scale, *p), label='Fitted function', color = 'gray')
-	
-	# Export image
-	plt.savefig('modelfit.png')
-
-	# Get RMSD 
-	rmsd = np.mean((actual_values - model(predicted_values, *p)) ** 2)
-
-	print("Model: ", model.__name__)
-	print("Optimal Model Parameters: ", p)
-	print("RMSD: ", rmsd)
 
 #####################################################################################################################################################################
+# DATA 
 
 # Hypothetical Data 
 # a_params = [0.10, 0.30, 0.50, 0.70, 0.90] 
 # v_params = [0.05, 0.2, 0.45, 0.75, 0.80] 
-# av_values = [0.05, 0.15, 0.25, 0.75, 0.95, 0.15, 0.35, 0.50, 0.70, 0.85, 0.10, 0.30, 0.50, 0.70, 0.90, 0.20, 0.40, 0.60, 0.80, 0.90, 0.30, 0.45, 0.75, 0.85, 0.95]
+# av_actual = [0.05, 0.15, 0.25, 0.75, 0.95, 0.15, 0.35, 0.50, 0.70, 0.85, 0.10, 0.30, 0.50, 0.70, 0.90, 0.20, 0.40, 0.60, 0.80, 0.90, 0.30, 0.45, 0.75, 0.85, 0.95]
 
 # Real Data 
-a_params = [0.01, 0.04, 0.23, 0.94, 0.99]
-v_params = [0.03, 0.44, 0.82, 0.93, 0.97]
-av_values = [0.02, 0.02, 0.07, 0.53, 0.76, 0.03, 0.09, 0.20, 0.81, 0.94, 0.13, 0.20, 0.46, 0.96, 0.99, 0.27, 0.41, 0.68, 0.96, 0.99, 0.28, 0.50, 0.70, 0.99, 0.99]
+a_original = [0.01, 0.04, 0.23, 0.94, 0.99]
+v_original = [0.03, 0.44, 0.82, 0.93, 0.97]
+av_original = [0.02, 0.02, 0.07, 0.53, 0.76, 0.03, 0.09, 0.20, 0.81, 0.94, 0.13, 0.20, 0.46, 0.96, 0.99, 0.27, 0.41, 0.68, 0.96, 0.99, 0.28, 0.50, 0.70, 0.99, 0.99]
 
-predicted_values = preprocessing(a_params, v_params)
-fitModel(predicted_values, av_values)
+#####################################################################################################################################################################
+# MAIN 
 
+# Format original data for later use
+original_params_list = a_original + v_original
+original_table = a_original + v_original + av_original
+original_prediction = generatePrediction(a_original, v_original)
 
+# Fit model (get optimal a and v parameters) and generate prediction from them 
+result = fitModel(original_params_list, original_table)
+a_result = result[0:5]
+v_result = result[5:10]
+optimized_prediction = generatePrediction(a_result, v_result)
 
+# Format optimal prediction for later use 
+a_opt = optimized_prediction[0:5]
+v_opt = optimized_prediction[5:10]
+av_opt = optimized_prediction[10:]
 
+# Print Tables
+print()
+print("Original")
+drawTable(a_original, v_original, av_original)
+print()
+print("Optimal")
+drawTable(a_opt, v_opt, av_opt)
+
+# Print Parameters
+print()
+print("Original A Values ", a_original)
+print("Optimal A Values ", a_opt)
+print()
+print("Original V Values", v_original)
+print("Optimal V Values", v_opt)
+
+# Print RMSDs
+print()
+print("Original RMSD")
+residuals = getResiduals(original_table, original_prediction)
+getRMSD(residuals)
+print()
+print("Optimized RMSD")
+residuals = getResiduals(original_table, optimized_prediction)
+getRMSD(residuals)
 
 
